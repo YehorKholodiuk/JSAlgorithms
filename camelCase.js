170a3b0f0cadf9e953ffead4cc42540168e7a926
ASUS <Yehor Kholodiuk>
2021-12-23 14:45:44 +0200

    initial commit

---
+++ camelCase.js
@@ -0,0 +1,9 @@
+//Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
+
+    //For instance:
+
+    ////"camel case word".camelCase() => CamelCaseWord
+String.prototype.camelCase=function() {
+    return this.split('').map(word=>word.charAt(0).toUpperCase() + word.slice(1)).join('');
+}
+
